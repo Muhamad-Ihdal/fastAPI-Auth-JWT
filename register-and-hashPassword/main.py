54f87cfb496fa914b827ff3fc34bbb5512f3a4b4
Muhammad-Ihdal <muhammadihdal300@gmail.com>
2025-12-24 10:24:40 +0700

    add register-and-hashPassword/main.py

--- register-and-hashPassword/main.py
+++ register-and-hashPassword/main.py
@@ -6,4 +6,19 @@ pwd_context = CryptContext(
 )
 
 def hash_password(password:str) -> str:
-    pass
+    return pwd_context.hash(password)
+
+def verify(inputan:str,pwd_asli:str) -> bool:
+    return pwd_context.verify(inputan,pwd_asli)
+
+create = hash_password(password="hello123")
+print("===========================")
+print(create)
+print("===========================")
+print("===========================")
+print("===========================")
+
+pwSalah = "halloworld"
+pwBenar = "hello123"
+print(verify(pwSalah,create))
+print(verify(pwBenar,create))
