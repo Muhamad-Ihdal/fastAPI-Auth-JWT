5bfdbf4dca146783ac562448436a0c8ddf782114
Muhammad-Ihdal <muhammadihdal300@gmail.com>
2025-12-26 20:54:18 +0700

    point 2

--- depends-and-OAuth2PasswordBearer/main.py
+++ depends-and-OAuth2PasswordBearer/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI,HTTPException
 from schemas import UserRegister,UserResponse
 from jose import jwt,JWTError
-from passlib.context import CryptContext
+from auth import verify_password,hash_password
+from db import add_user,get_user_by_id
 app = FastAPI()
+
+def error(status_code=401,massage="not valid"):
+    raise HTTPException(
+        status_code=status_code,
+        detail={
+            "success":False,
+            "massage":massage
+        }
+    )
 
 
 @app.post("/register")
-def login(user: UserRegister):
-    user = user.model_dump()
-
+def register(user: UserRegister):
+    # user = user.model_dump()
+    add = add_user(user.email,hash_password(user.password))
+    if not add:
+        error(massage="Email telah digunakan")
     
